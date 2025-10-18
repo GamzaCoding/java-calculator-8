a66fc7db10aa83b07a7f3c328fe3b84cb3a06461
seok <hyeongseok1995@gmail.com>
2025-10-18 19:19:20 +0900

    test: 숫자들의 합 계산 기능 테스트 작성

---
+++ src/test/java/calculator/model/CalculatorMachineTest.java
@@ -0,0 +1,21 @@
+package calculator.model;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorMachineTest {
+  @Test
+  void 숫자들의_합_계산_기능(){
+    //given
+    CalculatorMachine calculatorMachine = new CalculatorMachine();
+    List<Integer> numbers = List.of(1, 2, 3);
+    int expect = 6;
+
+    //when
+    int actual = calculatorMachine.sum(numbers);
+
+    //then
+    Assertions.assertThat(actual).isEqualTo(expect);
+  }
+}
