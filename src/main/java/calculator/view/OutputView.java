41f0df3dc2dd2f618c65b9b543ea31a9c862b627
seok <hyeongseok1995@gmail.com>
2025-10-18 20:19:39 +0900

    feat: 결과 출력 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,14 @@
+package calculator.view;
+
+public class OutputView {
+  private static final String INIT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+  private static final String RESULT_MESSAGE = "결과 : ";
+
+  public void init() {
+    System.out.println(INIT_MESSAGE);
+  }
+  public void printResult(int result) {
+    System.out.println(RESULT_MESSAGE + result);
+  }
+
+}
