2b8d7d29bd56720b987b74c123bc392aaa1a1069
seok <hyeongseok1995@gmail.com>
2025-10-19 14:10:50 +0900

    refactor: Separator 메서드 분리, 하드코딩 수정, 메서드명 수정

--- src/main/java/calculator/model/Separator.java
+++ src/main/java/calculator/model/Separator.java
@@ -11,6 +11,11 @@ import java.util.stream.Collectors;
 public class Separator {
   private static final String COMMA = ",";
   private static final String COLON = ":";
+  private static final String ZERO = "0";
+  private static final String BACK_SLASH = "\\";
+  private static final String END_OF_HEADER_MARK = "n";
+  private static final String DELIMITER = "|";
+  private static final int FRONT_INDEX_OF_CUSTOM_SEPARATOR = 2;
 
   private final List<String> separators = new ArrayList<>(Arrays.asList(COMMA, COLON));
 
@@ -26,30 +31,40 @@ public class Separator {
   }
 
   private List<String> customSeparate(String inputString) {
-    String customSeparate = extractCustomSeparate(inputString);
+    String customSeparate = extractCustomSeparator(inputString);
     separators.add(customSeparate);
 
-    String customInputString = inputString.substring(inputString.indexOf("n") + 1);
+    String customInputString = extractBodyFromInput(inputString);
 
     return defaultSeparate(customInputString);
   }
 
-  private String extractCustomSeparate(String inputString) {
-    int newlineIndex = inputString.indexOf("\\");
-    return inputString.substring(2, newlineIndex);
+  private static String extractBodyFromInput(String inputString) {
+    int endOfHeadIndex = inputString.indexOf(END_OF_HEADER_MARK);
+    int bodyIndex = endOfHeadIndex + END_OF_HEADER_MARK.length();
+    return inputString.substring(bodyIndex);
+  }
+
+  private String extractCustomSeparator(String inputString) {
+    int behindIndexOfCustomSeparator = inputString.indexOf(BACK_SLASH);
+    return inputString.substring(FRONT_INDEX_OF_CUSTOM_SEPARATOR, behindIndexOfCustomSeparator);
   }
 
   private List<String> defaultSeparate(String inputString) {
-    if(inputString.equals("\"\"")) {
-      return List.of("0");
+    if(inputString.equals(DOUBLE_QUOTATION_MARKS_INPUT_REGEX)) {
+      return List.of(ZERO);
     }
 
-    String regex = separators.stream()
-        .map(Pattern::quote)
-        .collect(Collectors.joining("|"));
+    String regex = changeSeparatorsToRegex();
 
     return Arrays.stream(inputString.split(regex))
         .map(String::trim)
         .collect(Collectors.toList());
+  }
+
+  private String changeSeparatorsToRegex() {
+    return separators.stream()
+        .map(Pattern::quote)
+        .collect(Collectors.joining(DELIMITER));
   }
 }
